80118ed2b6edcad8a8c50374202f37a82d1a0b30
SamWarden <SamWardenSad@gmail.com>
2023-01-03 19:05:08 +0300

    Add `build_mediator` and `create_mediator_builder` to infrastructure

---
+++ src/infrastructure/factories/mediator.py
@@ -0,0 +1,40 @@
+import logging
+from collections.abc import Callable
+
+from di.container import ContainerState
+from didiator import CommandDispatcherImpl, EventObserverImpl, Mediator, MediatorImpl, QueryDispatcherImpl
+from didiator.middlewares.di import DiMiddleware
+from didiator.middlewares.logging import LoggingMiddleware
+from didiator.utils.di_builder import DiBuilder
+
+from src.application.user.commands.create_user import CreateUser, CreateUserHandler
+from src.application.user.commands.delete_user import DeleteUser, DeleteUserHandler
+from src.application.user.commands.update_user import UpdateUser, UpdateUserHandler
+from src.application.user.queries.get_user_by_id import GetUserById, GetUserByIdHandler
+from src.application.user.queries.get_user_by_username import GetUserByUsername, GetUserByUsernameHandler
+from src.application.user.queries.get_users import GetUsers, GetUsersHandler
+from src.infrastructure.constants import REQUEST_SCOPE
+
+
+def build_mediator(di_builder: DiBuilder) -> Mediator:
+    middlewares = (LoggingMiddleware(level=logging.INFO), DiMiddleware(di_builder, cls_scope=REQUEST_SCOPE))
+    command_dispatcher = CommandDispatcherImpl(middlewares=middlewares)
+    query_dispatcher = QueryDispatcherImpl(middlewares=middlewares)
+    event_observer = EventObserverImpl(middlewares=middlewares)
+
+    mediator = MediatorImpl(command_dispatcher, query_dispatcher, event_observer)
+    mediator.register_command_handler(CreateUser, CreateUserHandler)
+    mediator.register_command_handler(UpdateUser, UpdateUserHandler)
+    mediator.register_command_handler(DeleteUser, DeleteUserHandler)
+    mediator.register_query_handler(GetUserById, GetUserByIdHandler)
+    mediator.register_query_handler(GetUserByUsername, GetUserByUsernameHandler)
+    mediator.register_query_handler(GetUsers, GetUsersHandler)
+    # mediator.register_event_handler(UserCreated, UserCreatedHandler)
+
+    return mediator
+
+
+def create_mediator_builder(mediator: Mediator) -> Callable[[ContainerState], Mediator]:
+    def _build_mediator(di_state: ContainerState) -> Mediator:
+        return mediator.bind(di_state=di_state)
+    return _build_mediator
