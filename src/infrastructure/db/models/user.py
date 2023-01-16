306384b28eb01a30e6e531d7899493626b4b549d
SamWarden <SamWardenSad@gmail.com>
2023-01-16 21:00:59 +0300

    Add table models to infrastructure

---
+++ src/infrastructure/db/models/user.py
@@ -0,0 +1,18 @@
+from uuid import UUID
+
+import sqlalchemy as sa
+from sqlalchemy.orm import Mapped, mapped_column
+from uuid_extensions import uuid7
+
+from .base import TimedBaseModel
+
+
+class User(TimedBaseModel):
+    __tablename__ = "users"
+    __mapper_args__ = {"eager_defaults": True}
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid7, server_default=sa.text("uuid_generate_v7()"))
+    username: Mapped[str | None] = mapped_column(unique=True)
+    first_name: Mapped[str]
+    last_name: Mapped[str | None]
+    deleted: Mapped[bool] = mapped_column(default=False, server_default=sa.text("FALSE"))
