c3d9c55aeaf582b3f3c92b6261cbfe494be105c9
SamWarden <SamWardenSad@gmail.com>
2023-01-16 21:24:04 +0300

    Add SQLAlchemyUoW implementation

---
+++ src/infrastructure/db/uow.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.application.common.base.interfaces.uow import UnitOfWork
+
+
+class SQLAlchemyUoW(UnitOfWork):
+    def __init__(self, session: AsyncSession) -> None:
+        self._session = session
+
+    async def commit(self) -> None:
+        await self._session.commit()
+
+    async def rollback(self) -> None:
+        await self._session.rollback()
