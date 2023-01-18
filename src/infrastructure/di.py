2d43715ea7092b176ff7d70393638fdeb9894bf0
SamWarden <SamWardenSad@gmail.com>
2023-01-19 01:34:45 +0300

    Add di setup

--- src/infrastructure/di.py
+++ src/infrastructure/di.py
@@ -5,11 +5,19 @@ from di.dependent import Dependent
 from di.executors import AsyncExecutor
 from didiator import CommandMediator, EventMediator, Mediator, QueryMediator
 from didiator.utils.di_builder import DiBuilder
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 
-from src.application.base.interfaces.mapper import Mapper
+from src.application.commands.common.interfaces.persistence import UserRepo
+from src.application.common.base.interfaces.mapper import Mapper
+from src.application.common.base.interfaces.uow import UnitOfWork
+from src.application.queries.common.interfaces.persistence import UserReader
 from src.infrastructure.constants import APP_SCOPE, REQUEST_SCOPE
+from src.infrastructure.db.main import build_sa_engine, build_sa_session, build_sa_session_factory
+from src.infrastructure.db.repositories.user import UserReaderImpl, UserRepoImpl
+from src.infrastructure.db.uow import SQLAlchemyUoW
 from src.infrastructure.factories.mediator import build_mediator
-from src.infrastructure.mapper.main import build_mapper
+from src.infrastructure.mapper.main import build_mapper, MapperImpl
 
 
 def setup_mediator_factory(
@@ -23,19 +31,22 @@ def setup_mediator_factory(
     di_builder.bind(bind_by_type(Dependent(mediator_factory, scope=scope), EventMediator))
 
 
-def setup_di_builder() -> DiBuilder:
+def init_di_builder() -> DiBuilder:
     di_container = Container()
     di_executor = AsyncExecutor()
     di_scopes = (APP_SCOPE, REQUEST_SCOPE)
     di_builder = DiBuilder(di_container, di_executor, di_scopes=di_scopes)
+    return di_builder
 
+
+def setup_di_builder(di_builder: DiBuilder) -> None:
     di_builder.bind(bind_by_type(Dependent(lambda *args: di_builder, scope=APP_SCOPE), DiBuilder))
     di_builder.bind(bind_by_type(Dependent(build_mapper, scope=APP_SCOPE), Mapper))
     setup_mediator_factory(di_builder, build_mediator, APP_SCOPE)
 
-    # di_builder.bind(bind_by_type(Dependent(build_config, scope=APP_SCOPE), Config))
-    # di_builder.bind(bind_by_type(Dependent(build_sa_engine, scope=APP_SCOPE), AsyncEngine))
-    # di_builder.bind(bind_by_type(Dependent(build_sa_pool, scope=APP_SCOPE), sessionmaker))
-    # di_builder.bind(bind_by_type(Dependent(build_sa_session, scope=REQUEST_SCOPE), AsyncSession))
-    # di_builder.bind(bind_by_type(Dependent(build_repo, scope=REQUEST_SCOPE), UserRepo))
-    return di_builder
+    di_builder.bind(bind_by_type(Dependent(build_sa_engine, scope=APP_SCOPE), AsyncEngine))
+    di_builder.bind(bind_by_type(Dependent(build_sa_session_factory, scope=APP_SCOPE), sessionmaker))
+    di_builder.bind(bind_by_type(Dependent(build_sa_session, scope=REQUEST_SCOPE), AsyncSession))
+    di_builder.bind(bind_by_type(Dependent(SQLAlchemyUoW, scope=REQUEST_SCOPE), UnitOfWork))
+    di_builder.bind(bind_by_type(Dependent(UserRepoImpl, scope=REQUEST_SCOPE), UserRepo, covariant=True))
+    di_builder.bind(bind_by_type(Dependent(UserReaderImpl, scope=REQUEST_SCOPE), UserReader, covariant=True))
