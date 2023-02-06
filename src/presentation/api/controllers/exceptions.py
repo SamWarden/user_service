020514b20fbfabea155ce03e982b9a353217b919
SamWarden <SamWardenSad@gmail.com>
2023-02-06 15:56:13 +0300

    refactor(user): refactor `UserIsDeleted`, add check

--- src/presentation/api/controllers/exceptions.py
+++ src/presentation/api/controllers/exceptions.py
@@ -4,6 +4,7 @@ from starlette.requests import Request
 from starlette.responses import JSONResponse
 
 from src.application.user.exceptions import UserIdAlreadyExists, UserIdNotExist, UsernameAlreadyExists, UsernameNotExist
+from src.domain.user.exceptions import UserIsDeleted
 from src.domain.user.value_objects.username import EmptyUsername, TooLongUsername, WrongUsernameFormat
 
 
@@ -11,13 +12,13 @@ def setup_exception_handlers(app: FastAPI) -> None:
     app.add_exception_handler(Exception, exception_handler)
 
 
-async def exception_handler(request: Request, exc: Exception) -> JSONResponse:
-    match exc:
+async def exception_handler(request: Request, err: Exception) -> JSONResponse:
+    match err:
         case TooLongUsername() | EmptyUsername() | WrongUsernameFormat():
             return JSONResponse(status.HTTP_400_BAD_REQUEST)
-        case UsernameAlreadyExists() | UserIdAlreadyExists():
-            return JSONResponse(status.HTTP_409_CONFLICT)
         case UserIdNotExist() | UsernameNotExist():
             return JSONResponse(status.HTTP_404_NOT_FOUND)
+        case UsernameAlreadyExists() | UserIdAlreadyExists() | UserIsDeleted():
+            return JSONResponse(status.HTTP_409_CONFLICT)
         case _:
             return JSONResponse(status.HTTP_500_INTERNAL_SERVER_ERROR)
