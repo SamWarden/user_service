11e04d22f02d89c5abcfc58833fbdbc183c80978
SamWarden <SamWardenSad@gmail.com>
2023-02-07 02:31:11 +0300

    feat(pagination): agg pagination for `get_users`

--- src/presentation/api/controllers/user.py
+++ src/presentation/api/controllers/user.py
@@ -1,16 +1,20 @@
 from uuid import UUID
 
 from didiator import CommandMediator, QueryMediator
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, Path, Query, status
 
 from src.application.common.interfaces.mapper import Mapper
 from src.application.user import dto
 from src.application.user.commands import CreateUser, DeleteUser
 from src.application.user.commands.update_user import UpdateUser, UpdateUserData
 from src.application.user.exceptions import UserIdAlreadyExists, UserIdNotExist, UsernameAlreadyExists, UsernameNotExist
+from src.application.user.interfaces.persistence import GetUsersOrder
 from src.application.user.queries import GetUserById, GetUserByUsername, GetUsers
+from src.domain.base.constants import Empty
 from src.domain.user.exceptions import UserIsDeleted
-from src.domain.user.value_objects.username import EmptyUsername, TooLongUsername, WrongUsernameFormat
+from src.domain.user.value_objects.username import (
+    EmptyUsername, MAX_USERNAME_LENGTH, TooLongUsername, WrongUsernameFormat,
+)
 from src.presentation.api.controllers import requests, responses
 from src.presentation.api.providers.stub import Stub
 
@@ -49,7 +53,7 @@ async def create_user(
     },
 )
 async def get_user_by_username(
-    username: str,
+    username: str = Path(max_length=MAX_USERNAME_LENGTH),
     mediator: QueryMediator = Depends(Stub(QueryMediator)),
 ) -> dto.User:
     user = await mediator.query(GetUserByUsername(username=username))
@@ -75,9 +79,18 @@ async def get_user_by_id(
     "/", description="Return all users",
 )
 async def get_users(
+    deleted: bool | None = None,
+    offset: int = Query(0, ge=0),
+    limit: int = Query(1000, ge=0, le=1000),
+    order: GetUsersOrder = GetUsersOrder.ASC,
     mediator: QueryMediator = Depends(Stub(QueryMediator)),
 ) -> responses.Users:
-    users = await mediator.query(GetUsers())
+    users = await mediator.query(GetUsers(
+        deleted=deleted if deleted is not None else Empty.UNSET,
+        offset=offset,
+        limit=limit,
+        order=order,
+    ))
     return responses.Users(users=users)
 
 
