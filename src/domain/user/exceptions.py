40e0c131ad11bc0210622673d84aa955de59e6b5
SamWarden <SamWardenSad@gmail.com>
2023-02-07 00:55:43 +0300

    fix(deletion): fix `UserIsDeleted` error, make it a dataclass

--- src/domain/user/exceptions.py
+++ src/domain/user/exceptions.py
@@ -1,5 +1,8 @@
+from dataclasses import dataclass
+
 from src.domain.base.exceptions import DomainException
 
 
+@dataclass(eq=False)
 class UserIsDeleted(RuntimeError, DomainException):
     user_id: int
