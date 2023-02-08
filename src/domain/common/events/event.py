e81ef2d93aedf464b05090612905be8c6355dd53
SamWarden <SamWardenSad@gmail.com>
2023-02-09 01:40:09 +0300

    refactor(value_objects): remove `event_id` and `Timestamp` value objects

--- src/domain/common/events/event.py
+++ src/domain/common/events/event.py
@@ -1,13 +1,14 @@
 from abc import ABC
 from dataclasses import dataclass, field
+from datetime import datetime
+from uuid import UUID
 
 import didiator
 
-from src.domain.common.value_objects.event_id import EventId
-from src.domain.common.value_objects.time import Timestamp
+from uuid6 import uuid7
 
 
 @dataclass(frozen=True)
 class Event(didiator.Event, ABC):
-    event_id: EventId = field(kw_only=True, default_factory=EventId.generate)
-    event_timestamp: Timestamp = field(kw_only=True, default_factory=Timestamp.now)
+    event_id: UUID = field(kw_only=True, default_factory=uuid7)
+    event_timestamp: datetime = field(kw_only=True, default_factory=datetime.utcnow)
