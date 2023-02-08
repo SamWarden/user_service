f87f0b8db0eda52455f65ef5972ee0c9eb4e19bd
SamWarden <SamWardenSad@gmail.com>
2023-02-08 22:38:20 +0300

    refactor(interfaces): refactor typehints for mapper and add UserRepo

--- src/application/common/interfaces/mapper.py
+++ src/application/common/interfaces/mapper.py
@@ -1,8 +1,8 @@
-from typing import Any, Protocol, Type, TypeVar
+from typing import Any, Protocol, TypeVar
 
 T = TypeVar("T")
 
 
 class Mapper(Protocol):
-    def load(self, data: Any, class_: Type[T]) -> T:
+    def load(self, data: Any, class_: type[T]) -> T:
         raise NotImplementedError
