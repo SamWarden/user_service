b06986d3f792693b93a4664da15ac1d8e9fa3ce1
SamWarden <SamWardenSad@gmail.com>
2023-02-10 21:54:14 +0300

    feat(message_broker): add message broker implementation to interact with RabbitMQ

--- src/domain/common/value_objects/__init__.py
+++ src/domain/common/value_objects/__init__.py
@@ -1,8 +1,6 @@
 from .base import BaseValueObject, ValueObject
-from .time import Timestamp
 
 __all__ = (
     "BaseValueObject",
     "ValueObject",
-    "Timestamp",
 )
--- src/domain/user/__init__.py
+++ src/domain/user/__init__.py
@@ -1,5 +1,7 @@
+from . import events
 from .entities import User
 
 __all__ = (
     "User",
+    "events",
 )
