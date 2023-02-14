8416457da29c2fee8f93268f19bfa0271689d960
SamWarden <SamWardenSad@gmail.com>
2023-02-14 03:43:46 +0300

    fix(converter): fix cast `Username` to `str`

--- src/infrastructure/mapper/user.py
+++ src/infrastructure/mapper/user.py
@@ -56,7 +56,7 @@ def convert_db_model_to_user_dto(user: models.User) -> dto.User:
 
     return dto.User(
         id=user.id,
-        username=cast(user.username, str),
+        username=cast(str, user.username),
         first_name=user.first_name,
         last_name=user.last_name,
     )
