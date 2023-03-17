a284870e29ceb5fa8db362677896be1c3fd8fbae
SamWarden <SamWardenSad@gmail.com>
2023-03-17 13:54:39 +0300

    Add documentation to base table models

--- src/infrastructure/db/models/base.py
+++ src/infrastructure/db/models/base.py
@@ -4,11 +4,11 @@ from sqlalchemy import MetaData, sql
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, registry
 
 convention = {
-    'ix': 'ix_%(column_0_label)s',
-    'uq': 'uq_%(table_name)s_%(column_0_N_name)s',
-    'ck': 'ck_%(table_name)s_%(constraint_name)s',
-    'fk': 'fk_%(table_name)s_%(column_0_N_name)s_%(referred_table_name)s',
-    'pk': 'pk_%(table_name)s',
+    "ix": "ix_%(column_0_label)s",  # INDEX
+    "uq": "uq_%(table_name)s_%(column_0_N_name)s",  # UNIQUE
+    "ck": "ck_%(table_name)s_%(constraint_name)s",  # CHECK
+    "fk": "fk_%(table_name)s_%(column_0_N_name)s_%(referred_table_name)s",  # FOREIGN KEY
+    "pk": "pk_%(table_name)s",  # PRIMARY KEY
 }
 
 mapper_registry = registry(metadata=MetaData(naming_convention=convention))
@@ -20,6 +20,9 @@ class BaseModel(DeclarativeBase):
 
 
 class TimedBaseModel(BaseModel):
+    """
+    An abstract base model that adds created_at and updated_at timestamp fields to the model
+    """
     __abstract__ = True
 
     created_at: Mapped[datetime.datetime] = mapped_column(nullable=False, server_default=sql.func.now())
